4976c909909dc0e257505fceb20cb91177467906
Punit Patel <punit.patel2601@gmail.com>
2022-11-02 22:08:00 -0600

    Setup the Environment (#1)

---
+++ src/application/providers/index.ts
@@ -0,0 +1,3 @@
+import { setupDb } from '../database';
+
+const db = setupDb();
---
+++ src/application/server.ts
@@ -0,0 +1,20 @@
+import { ApolloServer } from 'apollo-server';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+
+import { typeDefs } from './schema';
+// import { resolvers } from './resolvers';
+
+const PORT = 4001;
+
+const createApp = async (): Promise<void> => {
+  const server = new ApolloServer({
+    // schema: makeExecutableSchema({ typeDefs, resolvers }),
+    schema: makeExecutableSchema({ typeDefs }),
+    introspection: true,
+  });
+
+  server.listen({ port: PORT });
+  console.log(`Server running at http://localhost:${PORT}${server.graphqlPath}`);
+};
+
+createApp();
