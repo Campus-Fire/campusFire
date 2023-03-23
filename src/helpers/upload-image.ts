be2475d77a196c4af3ea1d316da6bcb55e49ee5d
Punit Patel <punit.patel@neofinancial.com>
2023-03-22 19:13:19 -0600

    Removed Cloudinary refs

--- src/helpers/upload-image.ts
+++ src/helpers/upload-image.ts
@@ -1,25 +1,25 @@
-import config from '../../config';
+// import config from '../../config';
 
-const cloudinary = require('cloudinary');
+// const cloudinary = require('cloudinary');
 
-cloudinary.config({
-  cloud_name: config.CLOUDINARY_CLOUD_NAME,
-  api_key: config.CLOUDINARY_API_KEY,
-  api_secret: config.CLOUDINARY_API_SECRET,
-});
+// cloudinary.config({
+//   cloud_name: config.CLOUDINARY_CLOUD_NAME,
+//   api_key: config.CLOUDINARY_API_KEY,
+//   api_secret: config.CLOUDINARY_API_SECRET,
+// });
 
-const upload = async (image: string, user: string): Promise<string> => {
-  try {
-    const result = await cloudinary.v2.uploader.upload(image, {
-      allowed_formats: ['jpg', 'jpeg', 'png'],
-      public_id: '',
-      folder: `${user}`,
-    });
+// const upload = async (image: string, user: string): Promise<string> => {
+//   try {
+//     const result = await cloudinary.v2.uploader.upload(image, {
+//       allowed_formats: ['jpg', 'jpeg', 'png'],
+//       public_id: '',
+//       folder: `${user}`,
+//     });
 
-    return `${result.url}`;
-  } catch (err) {
-    throw new Error('err');
-  }
-};
+//     return `${result.url}`;
+//   } catch (err) {
+//     throw new Error('err');
+//   }
+// };
 
-export { upload };
+// export { upload };
